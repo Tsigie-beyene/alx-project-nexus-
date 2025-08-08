9e064ee1e261ab99a77155c810aae6b860be68ab
Tsigie Beyene <119432900+Tsigie-beyene@users.noreply.github.com>
2025-08-08 12:50:07 -0700

    Update settings.py

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -15,7 +15,14 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-your-secret-key-here-chang
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,0.0.0.0,alx-project-nexus-qmnd.onrender.com'
+).split(',')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://alx-project-nexus-qmnd.onrender.com'
+]
 
 # Application definition
 INSTALLED_APPS = [
@@ -118,12 +125,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Security Settings
 if not DEBUG:
     # Security headers for production
+   SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_SECONDS = 31536000
     SECURE_REDIRECT_EXEMPT = []
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', 'True').lower() == 'true'
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     X_FRAME_OPTIONS = 'DENY'
